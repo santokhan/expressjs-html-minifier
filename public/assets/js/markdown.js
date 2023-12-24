be259d4d9a7ab30827cc9246a03cd4e4ae14752b
Santo <santokhan1999@gmail.com>
2023-12-24 21:05:26 +0600

    Fetch README.md by express fs.readFileSync

--- public/assets/js/markdown.js
+++ public/assets/js/markdown.js
@@ -1,20 +1,22 @@
-const { json } = require("body-parser")
-
-const markdownData = `
-# HTML Minifier by Santo
-POST HTML to the API URL and get minified HTML.
-`.trim();
-
-function renderMarkdown(params) {
-    const md = new markdownit();
-    if (md) {
-        const html = md.render(markdownData);
-        const makdownBody = document.querySelector(".markdown-body");
-        console.log(makdownBody);
-        if (makdownBody) {
-            makdownBody.innerHTML = html;
+function renderMarkdown() {
+    fetch(window.location.origin + "/api/markdown").then(res => res.json()).then(data => {
+        if (data) {
+            const markdown = data.markdown;
+            if (typeof markdown == "string") {
+                const md = new markdownit();
+                if (md) {
+                    const html = md.render(data.markdown);
+                    const makdownBody = document.querySelector(".render-markdown");
+                    if (makdownBody) {
+                        makdownBody.innerHTML = html;
+                        setTimeout(() => {
+                            hljs.highlightAll();
+                        }, 10);
+                    }
+                }
+            }
         }
-    }
+    }).catch(console.error())
 };
 
-// window.addEventListener("DOMContentLoaded", renderMarkdown);
+window.addEventListener("DOMContentLoaded", renderMarkdown);
